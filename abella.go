07297307a219e56de2669ffe2bdb836fa7c67f0f
JSansoP <61268003+JSansoP@users.noreply.github.com>
2022-01-12 15:38:08 +0100

    final

--- abella.go
+++ abella.go
@@ -9,7 +9,7 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-//Funció per imprimir l'e si n'hi ha hagut
+//Funció per imprimir l'error si n'hi ha hagut
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
@@ -17,6 +17,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	//Cream la connexió a RabbitMQ
 	connexio, e := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(e, "Connexió a RabbitMQ fallida.")
@@ -56,6 +57,7 @@ func main() {
 	)
 	failOnError(e, "Failed to set QoS")
 
+	//Consumició dels missatges: consumim de la coa de permisos
 	missatges, e := canal.Consume(
 		perms.Name,
 		"",
@@ -68,6 +70,7 @@ func main() {
 	failOnError(e, "Error a l'intentar registrar un consumidor.")
 
 	//Declaram l'Exchange pel qual l'os enviarà el missatge d'acabar a les abelles
+	//que es de tipus fanout
 	e = canal.ExchangeDeclare(
 		"final",
 		"fanout",
@@ -79,6 +82,7 @@ func main() {
 	)
 	failOnError(e, "No s'ha pogut declarar l'Exchange")
 
+	//Aquesta és una coa local que anirà associada a l'Exchange
 	local, e := canal.QueueDeclare(
 		"",
 		false,
@@ -88,6 +92,8 @@ func main() {
 		nil,
 	)
 	failOnError(e, "Error al declarar una coa")
+
+	//Associam la coa local a l'exchange "final".
 	e = canal.QueueBind(
 		local.Name,
 		"",
@@ -95,6 +101,7 @@ func main() {
 		false,
 		nil)
 	failOnError(e, "Error a l'enllaçar la coa")
+	//Consumim de la coa local que contendrà el missatge de acabament.
 	msgFinal, e := canal.Consume(
 		local.Name,
 		"",
@@ -105,16 +112,22 @@ func main() {
 		nil,
 	)
 
-	nomAbella := os.Args[1]
+	nomAbella := os.Args[1] //Agafam el nom de l'abella dels arguments de consola
 	forever := make(chan bool)
+	//Aquesta gorutina s'encarregarà de llegir els missatges de la coa de permisos, i publicarà, en cas de ser la abella número 10,
+	// un missatge a la coa dels avisos per avisar a l'os de que el pot ja està ple.
 	go func() {
 		for missatge := range missatges {
+			//Si el missatge prové de la coa de permisos
 			if missatge.RoutingKey == perms.Name {
+				//Produim la mel
 				log.Printf("L'abella %s produeix la mel nombre %s", nomAbella, missatge.Body)
 				missatge.Ack(false)
 				//Si és la abella que posa la mel 10, despertam a l'os
 				if string(missatge.Body[:]) == "10" {
-
+					//Publicam el missatge per la coa dels avisos, amb el nom de l'abella com a cos del missatge, així l'os
+					// pot saber quina abella l'ha despertat.
+					log.Printf("Som l'abella %s i despert a l'os", nomAbella)
 					e = canal.Publish(
 						"",
 						coaAvisos.Name,
@@ -126,15 +139,17 @@ func main() {
 							Body:         []byte(nomAbella),
 						})
 					failOnError(e, "Error a l'enviar a l'os el missatge de despertar.")
-					log.Printf("Som l'abella %s i despert a l'os", nomAbella)
 				}
-				time.Sleep(time.Duration(1+rand.Float64()*2) * time.Second)
+				//Feim un sleep per forçar l'intercalat
+				time.Sleep(time.Duration(rand.Float64()*3) * time.Second)
 			}
 		}
 	}()
 
+	//Aquesta gorutina analitza els missatges de la coa de finalització
 	go func() {
 		for msg := range msgFinal {
+			//Si el missatge no prové de la coa de permisos, és que prové de la coa local, i acabam la simulació
 			if msg.RoutingKey != perms.Name {
 				msg.Ack(false)
 				log.Printf("El pot de mel s'ha romput!")
--- os.go
+++ os.go
@@ -17,6 +17,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	//Cream la connexió a RabbitMQ
 	connexio, e := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(e, "Connexió a RabbitMQ fallida.")
@@ -55,6 +56,8 @@ func main() {
 		0,
 		false)
 	failOnError(e, "Failed to set QoS")
+
+	//Consumició dels missatges: consumim de la coa d'avisos, que contendrà els avisos per despertar a l'os
 	missatges, e := canal.Consume(
 		coaAvisos.Name,
 		"",
@@ -65,7 +68,7 @@ func main() {
 		nil)
 	failOnError(e, "No s'ha pogut registrar el Consumer")
 
-	//Declaram el canal de permisos (on l'os posarà els permisos)
+	//Declaram el canal de permisos (on l'os posarà els permisos per a que les abelles puguin produïr)
 	perms, e := canal.QueueDeclare(
 		"permisos",
 		true,
@@ -74,10 +77,10 @@ func main() {
 		false,
 		nil)
 	failOnError(e, "Error al declarar el canal de permisos.")
-
+	//Posam els 10 permisos inicials per a les abelles
 	for i := 1; i <= 10; i++ {
 		permis := strconv.Itoa(i)
-
+		//Publicam, on el cos de la publicació és el nombre del permís
 		e := canal.Publish(
 			"",
 			perms.Name,
@@ -89,16 +92,21 @@ func main() {
 				Body:         []byte(permis),
 			})
 		failOnError(e, "No s'ha publicat un permís.")
-		log.Printf("Enviat %s", permis)
+		//log.Printf("Enviat %s", permis)
 	}
 
+	log.Printf("L'os s'envà a dormir.")
+	time.Sleep(time.Duration(1+rand.Float64()*3) * time.Second)
 	forever := make(chan bool)
+	//El nombre de pics que l'ós ha menjat del pot
 	picsMenjat := 0
+	//Aquesta gorutina s'encarrega de llegir els missatges d'avís, i quan ja ha menjat tres pics, envia el missatge d'acabament a les abelles i l'os acaba.
 	go func() {
 		for msg := range missatges {
 			picsMenjat++
 			log.Printf("L'abella %s m'ha despertat. Menj %d/3.", msg.Body, picsMenjat)
 			time.Sleep(time.Duration(1+rand.Float64()*3) * time.Second)
+			//Si és el 3r pic que menja, envia el missatge de acabament a través de l'exchange i fa un Exit
 			if picsMenjat == 3 {
 				msg.Ack(false)
 				finiquito := "acaba"
@@ -114,9 +122,9 @@ func main() {
 				failOnError(e, "El missatge de finalització no s'ha publicat.")
 				os.Exit(0)
 			} else {
+				//Si no és el 3r pic, posa 10 missatges més dins la coa
 				for i := 1; i <= 10; i++ {
 					permis := strconv.Itoa(i)
-
 					e := canal.Publish(
 						"",
 						perms.Name,
@@ -128,9 +136,11 @@ func main() {
 							Body:         []byte(permis),
 						})
 					failOnError(e, "No s'ha publicat un permís.")
-					log.Printf("Enviat %s", permis)
+					//log.Printf("Enviat %s", permis)
 				}
 				msg.Ack(false)
+				log.Printf("L'os s'envà a dormir.")
+				time.Sleep(time.Duration(1+rand.Float64()*3) * time.Second)
 			}
 		}
 	}()
